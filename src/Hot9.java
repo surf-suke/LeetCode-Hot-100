7e17c09fc78de592fca72d6118f9dbfcf4db7cf5
suke <2684465272@qq.com>
2025-08-13 00:31:54 +0800

    commited Hot9.

--- src/Hot9.java
+++ src/Hot9.java
@@ -1,3 +1,42 @@
-public class Hot9 {
+// src/Hot9.java
+import java.util.ArrayList;
+import java.util.List;
 
+public class Hot9 {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();       //答案
+        if (s.length() < p.length()) return res;
+
+        int[] pCount = new int[26];
+        int[] sCount = new int[26];
+        //统计两个字符串频率
+        for (int i = 0; i < p.length(); i++) {
+            pCount[p.charAt(i) - 'a']++;
+            sCount[s.charAt(i) - 'a']++;
+        }
+        //如果一开始频率相等说明一定s包含一个异位词p
+        if (matches(pCount, sCount)) res.add(0);
+
+        for (int i = p.length(); i < s.length(); i++) {
+            sCount[s.charAt(i) - 'a']++;
+            sCount[s.charAt(i - p.length()) - 'a']--;
+            if (matches(pCount, sCount)) res.add(i - p.length() + 1);
+        }
+        return res;
+    }
+
+    private boolean matches(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "cbaebabacd", p = "abc";
+        List<Integer> list = new Hot9().findAnagrams(s, p);
+        for (Integer x : list) {
+            System.out.print(x + " ");
+        }
+    }
 }
