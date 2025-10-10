093c0bf4125168c731396f6647d337673e65dc66
suke <2684465272@qq.com>
2025-10-10 18:12:04 +0800

    commited Hot17.

---
+++ src/CommonArray/Hot17.java
@@ -0,0 +1,23 @@
+package CommonArray;
+
+public class Hot17 {
+    public int firstMissingPositive(int[] nums) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            while (nums[i] > 0 && nums[i] <= len && nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+        return len + 1;
+    }
+    public static void main(String[] args){
+        int[] nums={1,2,0};
+        System.out.println(new Hot17().firstMissingPositive(nums));
+    }
+}
