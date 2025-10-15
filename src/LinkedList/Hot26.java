6b5fb180186a9d232df01d53d81e0a3e15d100b7
suke <2684465272@qq.com>
2025-10-15 22:40:36 +0800

    环判定入口节点

--- src/LinkedList/Hot26.java
+++ src/LinkedList/Hot26.java
@@ -1,19 +1,44 @@
 package LinkedList;
 
 public class Hot26 {
-    public class ListNode{
+    public class ListNode {
         int val;
-        Hot25.ListNode next;
-        ListNode(){}
-        ListNode(int val){
-            this.val=val;
+        ListNode next;
+
+        ListNode() {
         }
-        ListNode (int val, Hot25.ListNode next){
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
         }
     }
-    public ListNode detectCycle(ListNode head) {
 
+    public ListNode detectCycle(ListNode head) {
+        if(head==null)
+            return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast!=null) {
+            slow = slow.next;
+            if(fast.next!=null){
+                fast = fast.next.next;
+            }
+            else
+                return null;
+            if (slow == fast) {
+                ListNode ptr=head;
+                while(ptr!=slow){
+                    ptr=ptr.next;
+                    slow=slow.next;
+                }
+                return ptr;
+            }
+        }
+        return null;
     }
 }
